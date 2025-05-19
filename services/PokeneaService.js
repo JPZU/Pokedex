33e730521e8e78767fe1a5a8e1c0aedd25561416
JPZU <juanpablozp@icloud.com>
2025-05-19 12:32:02 -0500

    API REST LISTA

--- services/PokeneaService.js
+++ services/PokeneaService.js
@@ -1,42 +1,39 @@
 const os = require("os");
-const pokenea = require("../data/pokenea"); // asumiendo que cargas desde archivo
+const pokeneaData = require("../data/pokenea");
 
 class PokeneaService {
   constructor() {
-    this.pokenea = pokenea;
+    this.pokenea = pokeneaData;
   }
 
+  /**
+   * Returns all records along with container information.
+   */
   getAllWithContainerInfo() {
-    let containerId = "unknown";
-    try {
-      containerId = os.hostname();
-    } catch (err) {
-      console.warn("Could not read container ID: ", err.message);
-    }
+    const containerId = os.hostname();
 
-    return [
-      {
-        container_id: containerId,
-        message: "All pokenea",
-        dataset: this.pokenea,
-      },
-    ];
+    return {
+      containerId,
+      message: "All Pokenea data",
+      data: this.pokenea,
+    };
   }
 
+  /**
+   * Returns only philosophical images and phrases.
+   */
   getImagesAndPhrases() {
-    const dataset = Object.values(this.pokenea).map(
+    const data = Object.values(this.pokenea).map(
       ({ image, philosophical_phrase }) => ({
         image,
         philosophical_phrase,
       })
     );
 
-    return [
-      {
-        message: "All pokenea images and phrases",
-        dataset,
-      },
-    ];
+    return {
+      message: "Pokenea images and phrases",
+      data,
+    };
   }
 }
 
